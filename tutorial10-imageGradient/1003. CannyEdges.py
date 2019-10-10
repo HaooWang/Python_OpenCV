acdef2f000224ab7e18a5fe8f8abe0c92662e39b
Hao WANG <18075102g@connect.polyu.hk>
2019-10-10 15:16:28 +0800

    10/9/2019 Basic OpenCV operations

--- tutorial10-imageGradient/1003. CannyEdges.py
+++ tutorial10-imageGradient/1003. CannyEdges.py
@@ -5,3 +5,23 @@
 # @Site    : 
 # @File    : 1003. CannyEdges.py
 # @Software: PyCharm
+
+import numpy as np
+import cv2 as cv
+
+from matplotlib import pyplot as plt
+
+
+img = cv.imread('../pictures/google.png',0)
+
+# canny edge detector
+edges = cv.Canny(img,50,150)
+
+# show images
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+# plt.show()
+plt.show()
