86a97c7f236b96c0d0f4726d1a54c0db8dafd680
HaoWANG <18075102g@connect.polyu.hk>
2020-12-20 22:19:42 +0800

    20/12/2020  OpenCV Non-max Suppression Algorithm

--- tutorial11-trackbar/trackbar_create and pos.py
+++ tutorial11-trackbar/trackbar_create and pos.py
@@ -1,78 +1,87 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2019/5/13 17:29
+# @Time    : 2020/6/24 11:26
 # @Author  : HaoWANG
 # @Site    : 
-# @File    : tutorial01.py
+# @File    : trackbar_create and pos.py
 # @Software: PyCharm
 
-import math
-import sys
-
 import cv2 as cv
+import sys
+import math
 
 
+# Living Vedio Image Capture
 def vedio_cap():
-	# camera vedio processing initialization
+	# initialization
 	keep_processing = True
 	camera_to_use = 0  # 0 if you have one camera, 1 or > 1 otherwise
-	
 	# define video capture object
 	cap = cv.VideoCapture()
-	windowName = "Live Camera Input" # define display window name
-	
+	# define display window name
+	windowName = "image"
+	# if command line arguments are provided try to read video_name
 	# otherwise default to capture from attached H/W camera
+	
 	if (((len(sys.argv) == 2) and (cap.open(str(sys.argv[1]))))
 			or (cap.open(camera_to_use))):
 		
 		# create window by name (note flags for resizable or not)
-		cv.namedWindow(windowName, cv.WINDOW_AUTOSIZE)
+		cv.namedWindow(windowName, cv.WINDOW_NORMAL)
+		# create track bar window in image show Window
+		cv.createTrackbar('R', 'image', 0, 255, callBack)
+		cv.createTrackbar('G', 'image', 0, 255, callBack)
+		cv.createTrackbar('B', 'image', 0, 255, callBack)
+		switch = '0:OFF\n1:ON'
+		cv.createTrackbar(switch, 'image', 0, 1,callBack)
+		
 		while (keep_processing):
 			# error detection #
 			if (cap.isOpened):
 				ret, frame = cap.read()
 			
+			r = cv.getTrackbarPos('R', 'image')
+			g = cv.getTrackbarPos('G', 'image')
+			b = cv.getTrackbarPos('B', 'image')
+			s = cv.getTrackbarPos(switch, 'image')
+			if s == 0:
+				frame[:] = 0
+			else:
+				frame[:] = [b, g, r]
+			
 			# start a timer (to see how long processing and display takes)
 			start_t = cv.getTickCount()
-			# frame = cv.flip(frame, 1)
+			frame = cv.flip(frame, 1)
 			# display image
 			cv.imshow(windowName, frame)
 			# stop the timer and convert to ms. (to see how long processing and display takes)
 			stop_t = ((cv.getTickCount() - start_t) / cv.getTickFrequency()) * 1000
 			# wait 40ms or less depending on processing time taken (i.e. 1000ms / 25 fps = 40 ms)
 			
-			key = cv.waitKey(max(2, 20 - int(math.ceil(stop_t)))) & 0xFF
+			key = cv.waitKey(max(2, 40 - int(math.ceil(stop_t)))) & 0xFF
 			# It can also be set to detect specific key strokes by recording which key is pressed
 			# e.g. if user presses "x" then exit
 			if (key == ord('x')):
 				keep_processing = False
 				print("frame shape: {}. ".format(frame.shape))
 				gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-				cv.imwrite("E:/Pycharm Projects/Python_OpenCV/pictures/frameGRAY.png",gray)
+				cv.imwrite("E:/Pycharm Projects/Python_OpenCV/pictures/frameGRAY.png", gray)
 				print("-- key == ord('x') --")
 		
 		# close all windows
-
 		cv.destroyWindow(windowName)
+		
+# call back function of creattrackbar()
+def callBack():
+	pass
+
+# main function
+def main():
+	vedio_cap()
 
 
-def get_image_info(image):
-	print(image.shape[:2])
-	print(image.shape)
-	print(type(image))
-	print(image.size)
+if __name__ == '__main__':
+    main()
 
+		
 
-print("--------- Information ------")
-# mac os
-# src = cv.imread("/Users/haowang/ML_CV_Py_worksapce/Python_OpenCV/pictures/coins.jpg")
-# windows
-# src = cv.imread("E:\Pycharm Projects\Python_opencv3\pictures\coins.jpg")
-
-# cv.namedWindow("Input Image", cv.WINDOW_AUTOSIZE)
-# get_image_info(src)
-vedio_cap()
-cv.waitKey(0)
-
-# destroy all windows
-cv.destroyAllWindows()
